2b88109b3b95302066aa6a97703c03d25e920541
DonHaKim <donha917@gmail.com>
2024-08-21 20:53:11 +0900

    init project

---
+++ src/main/java/com/dev/fotm/FotmApplication.java
@@ -0,0 +1,13 @@
+package com.dev.fotm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotmApplication.class, args);
+	}
+
+}
